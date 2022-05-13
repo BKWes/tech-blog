cedc0c70e003a869d4f1f0db2f75176c5a21f726
Weston <barnesweston@gmail.com>
2022-05-13 13:17:48 -0500

    add comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,3 +2,48 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 // /api/comments
+// GET all comments
+router.get('/', (req,res) => {
+    Comment.findAll({})
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST comment
+router.post('/', (req,res) => {
+    if (req.session) {
+        Comment.create({
+          comment_text: req.body.comment_text,
+          post_id: req.body.post_id,
+          // use the id from the session
+          user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+      }
+});
+
+// DELETE comment
+router.delete('/:id', (req,res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(400).json({ message: 'No comment found with this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
