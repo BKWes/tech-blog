3290066d17ca2e4ac2bb61d51ab975ca0d0e292c
Weston <barnesweston@gmail.com>
2022-05-13 13:08:24 -0500

    add comment and vote models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote, Comment };
